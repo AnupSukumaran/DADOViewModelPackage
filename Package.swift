3b6efc2ec0a206a2c990275d72652b05365cc8e1
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-03-26 16:33:52 +0530

    - 4:33 pm DADO - Preparing Login page Layout

--- Package.swift
+++ Package.swift
@@ -14,13 +14,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
          .package(url: "https://github.com/AnupSukumaran/SASStringAttributePack.git", from: "1.0.1"),
+         .package(url: "https://github.com/AnupSukumaran/SASLogger.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DADOViewModelPackage",
-            dependencies: []),
+            dependencies: ["SASStringAttributePack", "SASLogger"]),
         .testTarget(
             name: "DADOViewModelPackageTests",
             dependencies: ["DADOViewModelPackage"]),
